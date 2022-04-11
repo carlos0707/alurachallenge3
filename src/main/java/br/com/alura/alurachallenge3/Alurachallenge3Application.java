855b28ed2fc556a18cb20dd4d372eaaca60271dd
Carlos <carlos.jeronimo@allin.com.br>
2022-04-11 17:32:22 -0300

    initial repository

---
+++ src/main/java/br/com/alura/alurachallenge3/Alurachallenge3Application.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurachallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Alurachallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Alurachallenge3Application.class, args);
+	}
+
+}
